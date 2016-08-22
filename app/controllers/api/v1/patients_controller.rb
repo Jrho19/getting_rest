c44359dcb324ded07e4154b6f5213d959d31e03d
John Rhoden <jrho19.jr@gmail.com>
2016-08-21 20:08:27 -0400

    routes file update

--- app/controllers/api/v1/patients_controller.rb
+++ app/controllers/api/v1/patients_controller.rb
@@ -1,5 +1,9 @@
 class Api::V1::PatientsController < ApplicationController
   respond_to? :json
+  skip_before_action :verify_authenticity_token
+
+  def new
+  end
 
   def show
     render json: Patient.find(params[:id])
