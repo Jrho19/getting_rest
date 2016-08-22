e7f4c01da9927c2f7cb9eff728d582c3ebc28da3
John Rhoden <jrho19.jr@gmail.com>
2016-08-21 20:29:02 -0400

    correction adds proper root route

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   # routes
-  root "patients#new"
+  root "api/v1/patients#new"
   namespace :api, defaults: { format: :json } do
     namespace :v1 do
       resources :patients
