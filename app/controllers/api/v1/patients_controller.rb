e21a364c0c54d5c9612887a9003e64af0d509074
John Rhoden <jrho19.jr@gmail.com>
2016-08-21 20:16:43 -0400

    create and index action addition

--- app/controllers/api/v1/patients_controller.rb
+++ app/controllers/api/v1/patients_controller.rb
@@ -5,7 +5,26 @@ class Api::V1::PatientsController < ApplicationController
   def new
   end
 
+  def index
+    render json: Patient.all
+  end
+
   def show
     render json: Patient.find(params[:id])
   end
+
+  def create
+    patient = Patient.new(patient_params)
+
+    if patient.save
+      render json: patient, status: 201
+    else
+      render json: { errors: patient.errors }, status: 422
+    end
+  end
+
+  private
+    def patient_params
+      params.require(:patient).permit(:start_time, :end_time, :first_name, :last_name, :comments)
+    end
 end
